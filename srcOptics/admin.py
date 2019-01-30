5e26ca7741c80933991721ad0e1e3f16b3c35853
Austin Shafer <amshafe2@ncsu.edu>
2019-01-30 09:32:17 -0500

    Implemented most of dehaans feedback

--- srcOptics/admin.py
+++ srcOptics/admin.py
@@ -6,5 +6,6 @@ from .models import *
 admin.site.register(Organization)
 admin.site.register(Repository)
 admin.site.register(Account)
-admin.site.register(Admin)
+admin.site.register(Author)
 admin.site.register(Commit)
+admin.site.register(LoginCredential)
--- srcOptics/models.py
+++ srcOptics/models.py
@@ -1,36 +1,47 @@
 from django.db import models
 
-# commit points to owning repo
-class Commit(models.Model):
-    repo = models.ForeignKey('Repository', on_delete=models.SET_NULL, null=True)
-    sha = models.CharField(max_length=256, default='')
-    lines_added = models.IntegerField(default=0)
-    lines_removed = models.IntegerField(default=0)
+# ?? rather than have 'Account', you can use the built in Django user and
+# group models - definitely do this, because Django logins already work with
+# these
+class Organization(models.Model):
+    # parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True)
+    owner = models.TextField(max_length=32, default='')
+    name = models.TextField(max_length=32, default='')
+    admins = models.ManyToManyField('Account', related_name='admins')
+    members = models.ManyToManyField('Account', related_name='members')
+
+    def __str__(self):
+        return self.name
 
 class Repository(models.Model):
-    parent = models.ForeignKey('Organization', on_delete=models.SET_NULL, null=True)
-    url = models.CharField(max_length=256, default='')
-    name = models.CharField(max_length=32, default='')
+    parent = models.ForeignKey(Organization, on_delete=models.SET_NULL, null=True)
+    url = models.TextField(max_length=256, default='')
+    name = models.TextField(max_length=32, default='')
     
     def __str__(self):
         return self.name
 
-class Organization(models.Model):
-    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True)    
-    name = models.CharField(max_length=32, default='')
+class LoginCredential(models.Model):
+    repo = models.ForeignKey(Repository, on_delete=models.CASCADE)
+    username = models.TextField(max_length=32, default='')
+    password = models.TextField(max_length=128, default='')
+    
+class Commit(models.Model):
+    repo = models.ForeignKey(Repository, on_delete=models.CASCADE)
+    sha = models.TextField(max_length=256, default='')
+    lines_added = models.IntegerField(default=0)
+    lines_removed = models.IntegerField(default=0)
 
-    def __str__(self):
-        return self.name
+class Author(models.Model):
+    commit = models.ForeignKey(Commit, on_delete=models.CASCADE)
+    email = models.TextField(max_length=64, default='')
 
-class Admin(models.Model):
-    organizations = models.ForeignKey('Organization', on_delete=models.SET_NULL, null=True)
-    account = models.ForeignKey('Account', on_delete=models.SET_NULL, null=True)
-
+# ?? get rid of me
 class Account(models.Model):
-    organizations = models.ForeignKey('Organization', on_delete=models.SET_NULL, null=True, blank=True)
-    repos = models.ForeignKey('Repository', on_delete=models.SET_NULL, null=True, blank=True)
-    username = models.CharField(max_length=16, default='')
-    email = models.CharField(max_length=32, default='')
+    organizations = models.ForeignKey(Organization, on_delete=models.SET_NULL, null=True, blank=True)
+    repos = models.ForeignKey(Repository, on_delete=models.SET_NULL, null=True, blank=True)
+    username = models.TextField(max_length=16, default='')
+    email = models.TextField(max_length=32, default='')
 
     def __str__(self):
         return self.username
