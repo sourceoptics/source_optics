a868ed7e63dacd64bf02f16f3911aa05a8ddece9
Nick Wrenn <nlwrenn@ncsu.edu>
2019-02-04 15:55:42 +0000

    Tried to start parsing the file using regexes, but we are going to go with a well-known format (i.e. XML). Added some imports for postgres.

--- srcOptics/plugins/organizations/github.py
+++ srcOptics/plugins/organizations/github.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import re
+from srcOptics.models import Commit, Repository
 
 class Repository:
     def clone_repo(repo_url, work_dir, repo_name):
@@ -10,9 +11,17 @@ class Repository:
     def log_repo(repo_url, work_dir, repo_name):
         cmd = subprocess.Popen('cd ' + work_dir + '/' + repo_name + ';git log --all --stat', shell=True, stdout=subprocess.PIPE)
         regex = re.compile('^commit.*')
+        counter = 0
+        info = {}
         for line in cmd.stdout:
             line = line.decode('utf-8')
+            if counter == 1:
+                info['author'] = line.split()[1]
+                Commit.objects.create(**info)
+
             if regex.match(line):
+                info['sha'] = line.split()[1] 
+                counter += 1
                 print(line)
 
     def scan_repo(repo_url):
