54ac07194f618d71a59bb207499c4e6a10ae47a1
Pranesh Kamalakanthan <pkamala@ncsu.edu>
2019-02-04 11:19:21 -0500

    Formatted git log command to JSON

--- srcOptics/plugins/organizations/github.py
+++ srcOptics/plugins/organizations/github.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
-import re
+import json
+
 from srcOptics.models import Commit, Repository
 
 class Repository:
@@ -9,20 +10,12 @@ class Repository:
         os.system('git clone ' + repo_url + ' ' + work_dir + '/' + repo_name)
 
     def log_repo(repo_url, work_dir, repo_name):
-        cmd = subprocess.Popen('cd ' + work_dir + '/' + repo_name + ';git log --all --stat', shell=True, stdout=subprocess.PIPE)
-        regex = re.compile('^commit.*')
-        counter = 0
-        info = {}
+        json_log = '\'{"commit":"%H","author":"%an","author_date":"%ad","commit_date":"%cd"}\''
+        cmd = subprocess.Popen('cd ' + work_dir + '/' + repo_name + ';git log --pretty=format:' + json_log, shell=True, stdout=subprocess.PIPE)
+
         for line in cmd.stdout:
             line = line.decode('utf-8')
-            if counter == 1:
-                info['author'] = line.split()[1]
-                Commit.objects.create(**info)
-
-            if regex.match(line):
-                info['sha'] = line.split()[1] 
-                counter += 1
-                print(line)
+            print(line)
 
     def scan_repo(repo_url):
         work_dir = os.path.abspath(os.path.dirname(__file__).rsplit("/", 2)[0]) + '/work'
