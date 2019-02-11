5db1770e21c335faad0d7600d705368f3423f474
Austin Shafer <amshafe2@ncsu.edu>
2019-02-11 11:20:03 -0500

    Add LoginCredential support to addrepo
    Also work on some model feedback

--- srcOptics/management/commands/addrepo.py
+++ srcOptics/management/commands/addrepo.py
@@ -2,6 +2,9 @@ from django.core.management.base import BaseCommand, CommandError
 
 from srcOptics.plugins.organizations.github import Scanner
 
+from srcOptics.models import LoginCredential
+import getpass
+
 class Command(BaseCommand):
     help = 'Adds a repository to a queue'
 
@@ -9,5 +12,11 @@ class Command(BaseCommand):
         parser.add_argument('repo_url', type=str, help='Repository url')
         
     def handle(self, *args, **kwargs):
-        Scanner.scan_repo(kwargs['repo_url'])
+
+        # get the VC credentials and make a LoginCredential before cloning
+        username = input('Username: ')
+        password = getpass.getpass('Password: ')
+        cred = LoginCredential.objects.create(username=username, password=password)
+        
+        Scanner.scan_repo(kwargs['repo_url'], cred)
         #print(kwargs['repo_url'] + " added")
--- srcOptics/models.py
+++ srcOptics/models.py
@@ -1,30 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import Group, User
 
-# ?? rather than have 'Account', you can use the built in Django user and
-# group models - definitely do this, because Django logins already work with
-# these
-class Organization(models.Model):
-    # parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True)
-    name = models.TextField(max_length=32, blank=False)
-    admins = models.ManyToManyField(User, related_name='admins')
-
-    def __str__(self):
-        return self.name
-
+class LoginCredential(models.Model):
+    username = models.TextField(max_length=32, blank=False)
+    password = models.TextField(max_length=128,  blank=False)
+    
 class Repository(models.Model):
-    parent = models.ForeignKey(Organization, on_delete=models.SET_NULL, blank=False, null=True, related_name='orgs')
+    cred = models.ForeignKey(LoginCredential, on_delete=models.CASCADE, null=True)
     url = models.TextField(max_length=256, blank=False)
     name = models.TextField(max_length=32, blank=False)
     
     def __str__(self):
         return self.name
-
-class LoginCredential(models.Model):
-    repo = models.ForeignKey(Repository, on_delete=models.CASCADE)
-    username = models.TextField(max_length=32, blank=False)
-    password = models.TextField(max_length=128,  blank=False)
     
+class Organization(models.Model):
+    # parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True)
+    repos = models.ManyToManyField(Repository, blank=True)
+    name = models.TextField(max_length=32, blank=False)
+    admins = models.ManyToManyField(User, related_name='admins')
+    members = models.ManyToManyField(User, related_name='members')
+
+    def __str__(self):
+        return self.name
     
 class Author(models.Model):
     email = models.TextField(max_length=64, blank=False, null=True)
