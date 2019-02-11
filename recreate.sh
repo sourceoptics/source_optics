ea30e4a55e14c7b7b023a420ee81b11b32507ae2
Pranesh Kamalakanthan <pkamala@ncsu.edu>
2019-02-11 10:17:15 -0500

    created init command in management console

--- recreate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#databse needs to exist first
-dropdb srcopt
-createdb srcopt
-python manage.py migrate
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'password')" | python manage.py shell
---
+++ srcOptics/management/commands/init.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.core.management import call_command
+from django.contrib.auth import get_user_model
+import os
+
+
+class Command(BaseCommand):
+    help = 'Initializes DB and creates an admin account'
+        
+    def handle(self, *args, **kwargs):
+        os.system('dropdb srcopt; createdb srcopt')
+        call_command('migrate')
+        print('Create an admin account')
+        username = input('Username: ')
+        email = input('Email: ')
+        while True:
+            password = input('Password: ')
+            if password == '':
+                print('Password cannot be empty!')
+                continue
+            confirm = input('Password (again): ')
+            if password == confirm:
+                break
+            else:
+                print('Passwords do not match!')
+        User = get_user_model()
+        User.objects.create_superuser(username, email, password)
