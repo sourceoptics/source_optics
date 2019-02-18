5a52f604f28fc24c30388ec9ae5acbe70bc2485f
Ady Francis <afranci@ncsu.edu>
2019-02-18 10:11:15 -0500

    Fixed init.py

--- srcOptics/management/commands/init.py
+++ srcOptics/management/commands/init.py
@@ -10,12 +10,12 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('-e', '--easy', action='store_true', help='Run in easy mode. Creates account admin / password')
     def handle(self, *args, **kwargs):
-        subprocess.call('rm -rf work')
+        subprocess.call('rm -rf', shell=True)
         try:
-            subprocess.call('dropdb srcopt')
+            subprocess.call('dropdb srcopt', shell=True)
         except OSError:
             pass
-        subprocess.call('createdb srcopt')
+        subprocess.call('createdb srcopt', shell=True)
         call_command('makemigrations')
         call_command('migrate')
         if kwargs['easy']:
