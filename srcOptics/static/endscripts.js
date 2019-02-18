fe795c38071b72acad2bbd90d20ee6fdc50d8742
Pranesh Kamalakanthan <pkamala@ncsu.edu>
2019-02-18 11:28:12 -0500

    displaying commit data by repository to view

--- srcOptics/static/endscripts.js
+++ srcOptics/static/endscripts.js
@@ -1,9 +1,8 @@
-document.getElementById('repos').addEventListener('change', (event) => {
+document.getElementById('repos').onchange = function() {
     const Http = new XMLHttpRequest();
-    Http.open('GET', '/?repo=' + event.target.value);
+    Http.open('GET', '/repo/?id=' + event.target.value);
     Http.send();
     Http.onreadystatechange=(e)=>{
-      console.log(Http.responseText);
-        document.body.innerHTML = Http.responseText;
+      document.getElementById('commits').innerHTML = Http.responseText;
     }
-});
+};
