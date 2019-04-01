6a3d5fd382d30bfcd547c10fad48d71724213317
Ady Francis <afranci@ncsu.edu>
2019-04-01 09:42:05 -0400

    Added comments to graphs.py, cleaned up variable names.

--- srcOptics/views/graph.py
+++ srcOptics/views/graph.py
@@ -65,6 +65,7 @@ def create_bar_graph(title, x_axis_title, y_axis_title, x_axis_data, y_axis_data
     return element
 
 
+#Creates and returns the HTML for a scatter plot.
 def create_scatter_plot(title, x_axis_title, y_axis_title, x_axis_data, y_axis_data):
     # Create traces
     trace0 = go.Scatter(
@@ -91,28 +92,36 @@ def create_scatter_plot(title, x_axis_title, y_axis_title, x_axis_data, y_axis_d
     element = opy.plot(fig, auto_open=False, output_type='div')
     return element
 
-def commits_by_repo(request):
+def attributes_by_repo(request):
 
-
+    #list of possible attribute values to filter by. Defined in models.py for Statistic object
     attributes = Statistic.ATTRIBUTES
+    #start date query parameter
     start = request.GET.get('start')
+    #end date query parameterd
     end = request.GET.get('end')
+    #attribute query parameter
     attribute = request.GET.get('attr')
+    #search filter query parameter
     search = request.GET.get('filter')
 
+    #If there isn't a filter query parameter, list all repository organization
+    #TODO: filter by organization
     if not search:
         repos = Repository.objects.all()
     else:
+        #If there is a filter query parameter, list repositories which match the filter
+        #This includes repository name and repository tags
+        #Note that a filter "ta" will return repos with the tag "tag1"
         repos = Repository.objects.filter(name__icontains=search)
         tag_query = Tag.objects.filter(name__icontains=search)
         for tag in tag_query:
+            #Join the repos meeting the filter to the queryset
             repos |= tag.repos.all()
 
-    commits = []
-    names = []
 
 
-
+    #If start or end query parameters aren't defined, default to the last week starting from current day
     if not start or not end:
         end = datetime.now()
         start = end - timedelta(days=7)
@@ -120,30 +129,31 @@ def commits_by_repo(request):
         start = datetime.strptime(start, '%Y-%m-%d')
         end = datetime.strptime(end, '%Y-%m-%d')
 
-    # Default attribute if no query string is specified
+    # Default attribute(total commits)if no query string is specified
     if not attribute:
         attribute = Statistic.ATTRIBUTES[0][0]
-
     line_elements = ""
+    #Iterate over repo queryset
     for r in repos:
-        #commits over timezone
+        #array for dates
         dates = []
+        #array for attribute values
         attribute_by_date = []
-
-        names.append(r.name)
+        #Filter Rollup table for daily interval statistics for the current repository over the specified time range
         stats_set = Statistic.objects.filter(interval='DY', repo=r, author=None, start_date__range=(start, end))
         #aggregate_data = stats_set.aggregate(data=Sum(attribute))
 
+
+        #adds dates and attribute values to their appropriate arrays to render into graph data
         for stat in stats_set:
             dates.append(stat.start_date)
             attribute_by_date.append(getattr(stat, attribute))
 
-        #commits.append(aggregate_data['commit_total'])
+        #creates a scatter plot for each repository
         line_element = create_scatter_plot(r.name, "Date", attribute.replace("_", " ").title(), dates, attribute_by_date)
         line_elements = line_elements + line_element
 
 
-    #bar_element = create_bar_graph("Commits Per Repository", "Repository", "Commits", names, commits)
 
     context = {
         'title': "Repo Statistics Over Time",
--- srcOptics/views/urls.py
+++ srcOptics/views/urls.py
@@ -5,7 +5,7 @@ from . import views, graph, api
 urlpatterns = [
     path('', views.index),
     # path('repo/<str:repo_name>', graph.basic),
-    path('repos/', graph.commits_by_repo, name='repos'),
+    path('repos/', graph.attributes_by_repo, name='repos'),
     # path('repo_details/', views.repo_details, name='repo_details'),
     path('repos/<slug>/', views.repo_details, name='repo_details'),
     path('q/<query>/', api.search, name='api')
