87aaab5adade63ef16d363402f1b021dbb5430ef
Michael DeHaan <michael@michaeldehaan.net>
2019-09-18 12:29:55 -0400

    hide the author all-repos graph link as it is not currently implemented

--- source_optics/templates/tables.js
+++ source_optics/templates/tables.js
@@ -1,9 +1,9 @@
 function authorNav(value) {
    var index = "<a href='/author/" + value + "'><i class='fas fa-user'></i></a>&nbsp;";
-   var graphs = "<a href='/graphs?author=" + value + "&start={{ start_str }}&end={{ end_str }}&intv={{ intv }}'><i class='fas fa-chart-line'></i></a>&nbsp;";
+   //var graphs = "<a href='/graphs?author=" + value + "&start={{ start_str }}&end={{ end_str }}&intv={{ intv }}'><i class='fas fa-chart-line'></i></a>&nbsp;";
    var stats  = "<a href='/report/stats?author=" + value + "&start={{ start_str }}&end={{ end_str }}'><i class='fas fa-table'></i></a>&nbsp;";
    var commit_feed = "<a href='/report/commits?author=" + value + "&start={{ start_str }}&end={{ end_str }}'><i class='fas fa-rss-square'></i></a>&nbsp;";
-   return index + graphs + stats + commit_feed;
+   return index + stats + commit_feed;
 }
 
 function repoNav(value) {
